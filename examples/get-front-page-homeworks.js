04e3211d93fa88f1dfb12bb809d30e5632387c73
Gabriel Kleemann Duarte <gabrielkduarte234@gmail.com>
2021-08-17 00:04:46 -0300

    alteração

--- examples/get-front-page-homeworks.js
+++ examples/get-front-page-homeworks.js
@@ -3,8 +3,8 @@ const sigaa = new Sigaa({
   url: 'https://sigaa.ifsc.edu.br'
 });
 // coloque seu usuário
-const username = 'gabriel.kd';
-const password = '25dmpssX';
+const username = '';
+const password = '';
 
 const main = async () => {
   const account = await sigaa.login(username, password); // login
